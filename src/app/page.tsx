c6644fe6cdbd30550768360e45811deebe1acace
nimra <nimratabbassum3@gmail.com>
2025-09-04 14:52:19 +0500

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
-<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/img.jpg" alt="" width="384" height="512"/>
+import React from 'react'
+import Image from 'next/image'
+
+const page = () => {
+  return (
+    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <Image
+      src="/img.jpg"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+    />
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
+         &quot;making my next.js project.&quot;
       </p>
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        nimra tabbassum
+        Sarah Dayan
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-        paib
-      
+        Staff Engineer, Algolia
       </div>
     </figcaption>
   </div>
-</figure>
+</figure></div>
+  )
+}
+
+export default page
